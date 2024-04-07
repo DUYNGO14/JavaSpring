5ba304ce9df1ee136e9f169fc3ac340562332ab6
Ngô Duy <ngoduy07142003@gmail.com>
2024-04-07 09:45:59 +0700

    first

---
+++ 01-java-spring-laptopshop-starter/src/main/java/vn/hoidanit/laptopshop/service/UserService.java
@@ -0,0 +1,41 @@
+package vn.hoidanit.laptopshop.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.UserRepository;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public String handleHello() {
+        return "Hello from service";
+    }
+
+    public List<User> getAllUser() {
+        return this.userRepository.findAll();
+    }
+
+    public List<User> getAllUserByEmail(String email) {
+        return this.userRepository.findByEmail(email);
+    }
+
+    public User getAllUserById(long id) {
+        return this.userRepository.findOneById(id);
+    }
+
+    public User handleSaveUser(User user) {
+        return this.userRepository.save(user);
+    }
+
+    public void deleteUser(long id) {
+        this.userRepository.deleteById(id);
+    }
+}
