7b4d427169cba1195ab75e08572817a04f1a54df
Ngô Duy <ngoduy07142003@gmail.com>
2024-05-14 11:04:54 +0700

    #142 fetch product

--- java-spring-laptopshop/src/main/java/vn/hoidanit/laptopshop/controller/admin/ProductController.java
+++ java-spring-laptopshop/src/main/java/vn/hoidanit/laptopshop/controller/admin/ProductController.java
@@ -41,6 +41,9 @@ public class ProductController {
         Page<Product> prs = this.productService.fetchProducts(pageable);
         List<Product> listProducts = prs.getContent();
         model.addAttribute("products", listProducts);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", prs.getTotalPages());
+
         return "admin/product/show";
     }
 
